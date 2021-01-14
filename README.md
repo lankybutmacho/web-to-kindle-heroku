0b0c3e0864e3f437a9b71252fa9646b90cdc81d8
Matt Healy <matt@pickaxe.nyc>
2021-01-13 21:19:39 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Web to Kindle
+
+## Running Locally
---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const path = require('path');
+const puppeteer = require('puppeteer');
+const execFile = require('child_process').execFile;
+const fs = require('fs');
+
+const PORT = process.env.PORT || 3000;
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', async (req, res) => {
+    const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+    const page = await browser.newPage();
+    await page.setViewport({ width: 600, height: 800 });
+    await page.goto(process.env.SCREENSHOT_URL || 'https://darksky.net/details/40.7127,-74.0059/2021-1-6/us12/en');
+    await page.screenshot({
+      path: '/tmp/screenshot.png',
+    });
+
+    await browser.close();
+
+    await convert('/tmp/screenshot.png');
+    screenshot = fs.readFileSync('/tmp/screenshot.png');
+
+    res.writeHead(200, {
+      'Content-Type': 'image/png',
+      'Content-Length': screenshot.length,
+    });
+    return res.end(screenshot);
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+
+function convert(filename) {
+  return new Promise((resolve, reject) => {
+    const args = [filename, '-gravity', 'center', '-extent', '600x800', '-colorspace', 'gray', '-depth', '8', filename];
+    execFile('convert', args, (error, stdout, stderr) => {
+      if (error) {
+        console.error({ error, stdout, stderr });
+        reject();
+      } else {
+        resolve();
+      }
+    });
+  });
+}
