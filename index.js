c35d0b7ddf375571db6593fcb959ac41a6766112
kreativitat <37603265+kreativitat@users.noreply.github.com>
2023-03-25 19:40:52 +0000

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ express()
 
 function convert(filename) {
   return new Promise((resolve, reject) => {
-    const args = [filename, '-gravity', 'center', '-extent', '600x800', '-colorspace', 'gray', '-depth', '8', filename];
+    const args = [filename, '-gravity', 'center', '-extent', '300x1448', '-colorspace', 'gray', '-depth', '8', filename];
     execFile('convert', args, (error, stdout, stderr) => {
       if (error) {
         console.error({ error, stdout, stderr });
