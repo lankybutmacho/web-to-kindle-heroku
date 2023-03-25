301ce91d494f70aee6c13abb59540ec1810dd295
kreativitat <37603265+kreativitat@users.noreply.github.com>
2023-03-25 21:35:09 +0000

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ express()
     const page = await browser.newPage();
     await page.setViewport({ width: 1072, height: 1448 });
     await page.goto(process.env.SCREENSHOT_URL || 'https://darksky.net/details/40.7127,-74.0059/2021-1-6/us12/en');
-    // wait 10 sec
-    //await page.waitFor(10000);
+    // wait 5 sec
+    await page.waitFor(5000);
     //await page.waitForNavigation({ waitUntil: 'networkidle0' });
   
     await page.screenshot({
